c35700e2c9c07848e5c846e8a88f5822f7b6c53c
shaasa <info@beatriceweb.it>
2024-05-01 21:55:11 +0200

    feat(breeze): aggiunto breeze

--- README_VACANZARE.md
+++ README_VACANZARE.md
@@ -3,3 +3,8 @@
 1. ``` composer install ```
 2. ``` npm install ```
 3.  Creare il file .env
+4. Creare un db e configurare adeguatamente il file .env
+5. ```php artisan key:generate```
+6. ```php artisan serve``` per far partire il server in locale
+7. ```php artisan breeze:install``` (livewire functionality + darkmode)
+8. ```npm install```
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view('/', 'welcome');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
