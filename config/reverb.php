9ee3c853f075a396b35c81fb0182669e3b0a6ce9
bbartoli <beatrice.bartoli@immobiliare.it>
2024-05-24 13:53:58 +0200

    fix(config): svuota array 'tls' in config/reverb.php

--- config/reverb.php
+++ config/reverb.php
@@ -41,7 +41,7 @@ return [
             'port' => env('REVERB_SERVER_PORT', 8080),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => $tls,
+                'tls' => [],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
