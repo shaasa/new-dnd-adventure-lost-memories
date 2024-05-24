d8b019803d89c1f7c2d53f4b9eeef3a94a047569
bbartoli <beatrice.bartoli@immobiliare.it>
2024-05-25 01:30:47 +0200

    feat: aggiorna la configurazione di Reverb - Assegna il valore di `$tls` all'opzione 'tls' - Rimuove l'autorizzazione a tutte le origini da 'allowed_origins'

--- config/reverb.php
+++ config/reverb.php
@@ -41,7 +41,7 @@ return [
             'port' => env('REVERB_SERVER_PORT', 8080),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => [],
+                'tls' => $tls,
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
@@ -88,7 +88,7 @@ return [
                     'scheme' => env('REVERB_SCHEME', 'https'),
                     'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
                 ],
-                'allowed_origins' => ['*'],
+                'allowed_origins' => [],
                 'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
                 'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
             ],
